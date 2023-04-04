effa8c736904d3a226430c4965784a7283b0e386
dtoes62 <edick@regis.edu>
2023-04-03 20:44:20 -0400

    Evan Edit

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-<<<<<<< HEAD
+HEAD
 	  res.end('Hello World. My name is Evan D... I just changed this message for you\n');
-=======
-	  res.end('Hello World. My name is Tyler... I just changed this message for you\n');
->>>>>>> 1322dbf66dd547dd914e52507864fb128325ced3
+
+1322dbf66dd547dd914e52507864fb128325ced3
 });
 
 server.listen(port, hostname, () => {
